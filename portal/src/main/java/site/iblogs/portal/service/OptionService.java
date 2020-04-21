f4a4f59607985dd164bfbda49cfd84cba49270f5
Zhenyu,Liu <liuzhenyulive@live.com>
2020-04-21 23:09:09 +0800

    add mapstruct

--- portal/src/main/java/site/iblogs/portal/service/OptionService.java
+++ portal/src/main/java/site/iblogs/portal/service/OptionService.java
@@ -1,9 +1,11 @@
 package site.iblogs.portal.service;
 
+import site.iblogs.common.model.ConfigKey;
 import site.iblogs.model.Options;
 
 import java.util.List;
 
 public interface OptionService {
     List<Options> getAllOption();
+    String getOption(ConfigKey key);
 }
--- portal/src/main/java/site/iblogs/portal/service/impl/OptionServiceImpl.java
+++ portal/src/main/java/site/iblogs/portal/service/impl/OptionServiceImpl.java
@@ -1,7 +1,9 @@
 package site.iblogs.portal.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import site.iblogs.common.model.ConfigKey;
 import site.iblogs.mapper.OptionsMapper;
 import site.iblogs.model.Options;
 import site.iblogs.model.OptionsExample;
@@ -19,25 +21,39 @@ public class OptionServiceImpl implements OptionService {
     @Autowired
     private RedisService redisService;
 
-    private static boolean initializedOptionsToRedis=false;
-    private static String optionsPreKey="IBLOGS.SITE.OPTIONS";
+    private static boolean initializedOptionsToRedis = false;
+    private static String optionsPreKey = "IBLOGS.SITE.OPTIONS";
 
-    public OptionServiceImpl(){
-        if(!initializedOptionsToRedis){
+    public OptionServiceImpl() {
+        if (!initializedOptionsToRedis) {
             InitOptionsToRedis();
         }
     }
 
-    private void InitOptionsToRedis()
-    {
-        if(initializedOptionsToRedis){
+    private void InitOptionsToRedis() {
+        if (initializedOptionsToRedis) {
             return;
         }
-        List<Options> allOptions=getAllOption();
-        allOptions.forEach(u->{
-            redisService.set(optionsPreKey+u.getName(),u.getValue());
+        List<Options> allOptions = getAllOption();
+        allOptions.forEach(u -> {
+            redisService.set(optionsPreKey + u.getName(), u.getValue());
         });
-        initializedOptionsToRedis=true;
+        initializedOptionsToRedis = true;
+    }
+
+    @Override
+    public String getOption(ConfigKey key) {
+        String value = redisService.get(key.name());
+        if (StringUtils.isEmpty(value)) {
+            OptionsExample example = new OptionsExample();
+            List<Options> options = optionsMapper.selectByExample(example);
+            if (options.size() > 0) {
+                redisService.set(optionsPreKey + options.get(0).getName(), options.get(0).getValue());
+            } else {
+                return null;
+            }
+        }
+        return value;
     }
 
     @Override
