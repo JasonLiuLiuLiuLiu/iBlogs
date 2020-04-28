9303feab0a72398677e9580c108acf3ccbe31e73
Zhenyu,Liu <liuzhenyulive@live.com>
2020-04-28 22:54:24 +0800

    add metadata api

--- portal/src/main/java/site/iblogs/portal/controller/MetaDataController.java
+++ portal/src/main/java/site/iblogs/portal/controller/MetaDataController.java
@@ -1,21 +1,37 @@
 package site.iblogs.portal.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import site.iblogs.common.api.ApiResponse;
+import site.iblogs.common.api.PageResponse;
+import site.iblogs.portal.model.params.MetaDataType;
 import site.iblogs.portal.model.response.MetaDataResponse;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import site.iblogs.portal.service.MetadataService;
 
 @Api(tags = "MetaDataController", value = "分类,标签")
 @Controller
 @RequestMapping("/metadata")
 public class MetaDataController {
-    public ApiResponse<MetaDataResponse> AllCategories() {
-        throw new NotImplementedException();
+
+    @Autowired
+    private MetadataService metadataService;
+
+    @ApiOperation("获取分类")
+    @RequestMapping(value = "categories", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResponse<PageResponse<MetaDataResponse>> Categories(int pageNum, int pageSize) {
+        return ApiResponse.success(metadataService.getMetadata(MetaDataType.Category,pageNum,pageSize));
     }
 
-    public ApiResponse<MetaDataResponse> AllTags() {
-        throw new NotImplementedException();
+    @ApiOperation("获取标签")
+    @RequestMapping(value = "tags", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResponse<PageResponse<MetaDataResponse>> Tags(int pageNum, int pageSize) {
+        return ApiResponse.success(metadataService.getMetadata(MetaDataType.Tag,pageNum,pageSize));
     }
 }
--- portal/src/main/java/site/iblogs/portal/model/response/MetaDataResponse.java
+++ portal/src/main/java/site/iblogs/portal/model/response/MetaDataResponse.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Map;
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class MetaDataResponse {
-    public int total;
-    public Map<String,Integer> data;
+    private String name;
+    private int count;
 }
