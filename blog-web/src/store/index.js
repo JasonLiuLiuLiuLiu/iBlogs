f3a16deea054890d1f9f08ec423cbe276cd80f3a
Zhenyu,Liu <liuzhenyulive@live.com>
2020-05-19 00:22:25 +0800

    add options to store

--- blog-web/src/store/index.js
+++ blog-web/src/store/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import app from './modules/app'
+import options from "./modules/options";
 import getters from './getters'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    app
+    app,
+    options
   },
   getters
 })
---
+++ blog-web/src/store/modules/options.js
@@ -0,0 +1,23 @@
+const options = {
+  state: {
+    FootContent1: "页脚1",
+    FootContent2: "页脚2",
+    Announcement: "通知公告",
+    ContentCount: 0,
+    CommentCount: 0,
+    SiteInstallTime: '2020-01-01',
+    LastActiveTime: '2020-01-01',
+    SiteUrl: 'http://localhost:8090/',
+    pageSize: 20,
+    SideBarCategoriesCount: 5,
+    hotCategories:[],
+    SideBarTagsCount: 5,
+    hotTags:[],
+    friendlyLinks:[{title:'LiuZhenYu',link:'https://github.com/liuzhenyulive'}],
+    Author: 'iBlogs'
+  },
+  mutations: {},
+  actions: {}
+}
+
+export default options
