64174b0f492936ffe55259f5e25f41a7927f6f89
Zhenyu,Liu <liuzhenyulive@live.com>
2020-05-22 00:59:34 +0800

    Update Share.vue

--- blog-web/src/views/article/components/Share.vue
+++ blog-web/src/views/article/components/Share.vue
@@ -4,16 +4,16 @@
       <div class="bottom-bar-items social-share left">
         <span class="bottom-bar-item">Share : </span>
         <span class="bottom-bar-item bottom-bar-facebook"><a
-          href="https://www.facebook.com/sharer/sharer.php?u=@ViewService.SiteUrl()@Model.Content.Permalink()"
+          :href="'https://www.facebook.com/sharer/sharer.php?u='+this.$store.state.options.options.SiteUrl+'/article/'+this.$store.state.content.content.id"
           target="_blank" title="@Model.Content.Title" rel="nofollow">facebook</a></span>
         <span class="bottom-bar-item bottom-bar-twitter"><a
-          href="https://twitter.com/intent/tweet?url=@ViewService.SiteUrl()@Model.Content.Permalink()&text=@Model.Content.Title"
+          :href="'https://twitter.com/intent/tweet?url='+this.$store.state.options.options.SiteUrl+'/article/'+this.$store.state.content.content.id+'&text='+encodeURIComponent(this.$store.state.content.content.title)"
           target="_blank" title="@Model.Content.Title" rel="nofollow">Twitter</a></span>
         <span class="bottom-bar-item bottom-bar-weibo"><a
-          href="http://service.weibo.com/share/share.php?url=@ViewService.SiteUrl()@Model.Content.Permalink()&amp;title=@Model.Content.Title"
+          :href="'http://service.weibo.com/share/share.php?url='+this.$store.state.options.options.SiteUrl+'/article/'+this.$store.state.content.content.id+'&amp;title='+encodeURIComponent(this.$store.state.content.content.title)"
           target="_blank" title="@Model.Content.Title" rel="nofollow">Weibo</a></span>
         <span class="bottom-bar-item bottom-bar-qrcode"><a
-          href="https://cli.im/api/qrcode/code?text=@ViewService.SiteUrl()@Model.Content.Permalink()&mhid=sELPDFnok80gPHovKdI"
+          :href="'https://cli.im/api/qrcode/code?text='+this.$store.state.options.options.SiteUrl+'/article/'+this.$store.state.content.content.id"
           target="_blank" rel="nofollow">QRSetCode</a></span>
       </div>
       <div class="bottom-bar-items">
@@ -29,7 +29,13 @@
 </template>
 <script>
   export default {
-    name: 'Share'
+    name: 'Share',
+    date() {
+      return {
+        siteUrl: this.$store.state.options.options.SiteUrl,
+        contentId: this.$store.state.content.content.id
+      }
+    }
   }
 </script>
 <style>
