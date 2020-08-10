d1f06c80500b2f8362000b1b788427263dddb497
Zhenyu,Liu <liuzhenyulive@live.com>
2020-08-11 00:19:41 +0800

    add spring task

---
+++ blog-portal/src/main/java/site/iblogs/portal/component/SiteMapUpdateTask.java
@@ -0,0 +1,12 @@
+package site.iblogs.portal.component;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SiteMapUpdateTask {
+    @Scheduled(fixedDelay = 3000)
+    public void Upload(){
+        System.out.println("Upload xml file to ftp");
+    }
+}
